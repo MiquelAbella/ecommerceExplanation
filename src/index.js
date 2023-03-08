ab15f247030ef4828e90c48cf7facb992a151eb2
Miquel <abellagarciamiquel@gmail.com>
2023-03-08 10:50:51 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Products } from "./pages/Products";
+import { Cart } from "./pages/Cart";
+import { CartProvider } from "./context/CartContext";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <CartProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  </CartProvider>
+);
+
+reportWebVitals();
